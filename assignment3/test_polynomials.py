b5d04731adb98bc1c986f71cae819eb3e57935a4
Evgenia Gurova <izhenka@gmail.com>
2017-09-14 22:32:48 +0200

    Some tests added

--- assignment3/test_polynomials.py
+++ assignment3/test_polynomials.py
@@ -17,11 +17,24 @@ def test_add_polynomials():
     p2 = Polynomial([1, 2, 3, 4])
     assert (p1 + p2) = Polynomial([2, 4, 6, 4])
 
-def test_add_polynomials():
+def test_add_polynomials_with_zeroes():
+    p1 = Polynomial([0, 2, 0])
+    p2 = Polynomial([1, 0, 3, 0])
+    assert (p1 + p2) = Polynomial([1, 2, 3])
+
+def test_subtract_polynomials():
     p1 = Polynomial([1, 2, 3])
     p2 = Polynomial([1, 2, 3, 4])
     assert (p1 - p2) = Polynomial([0, 0, 0, -4])
 
-def test_polynomial_evaluate_minus_2():
+def test_degree_no_zeroes():
     p = Polynomial([1, 2, 3, 4, 5])
     assert p.degree() = 4
+
+def test_degree_highest_zero():
+    p = Polynomial([1, 2, 3, 4, 0])
+    assert p.degree() = 3
+
+def test_degree_all_zeroes():
+        p = Polynomial([0, 0, 0, 0, 0])
+        assert p.degree() = 0
