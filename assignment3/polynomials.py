5d8c96d018651cfe54f2339536cd215971059722
Evgenia Gurova <izhenka@gmail.com>
2017-09-15 18:28:38 +0200

    Some functions in polynomials and tests

--- assignment3/polynomials.py
+++ assignment3/polynomials.py
@@ -1,42 +1,48 @@
 class Polynomial:
 
     def __init__(self, coefficients):
-        """coefficients should be a list of numbers with 
+        """coefficients should be a list of numbers with
         the i-th element being the coefficient a_i."""
-        
-        raise NotImplemented
+
+        self.coefficients = coefficients
 
     def degree(self):
         """Return the index of the highest nonzero coefficient.
         If there is no nonzero coefficient, return -1."""
-        
-        raise NotImplemented
+
+        for index in reversed(range(len(self.coefficients))):
+            if self.coefficients[index] != 0:
+                return index
+
+        return -1;
+
 
     def coefficients(self):
-        """Return the list of coefficients. 
+        """Return the list of coefficients.
 
-        The i-th element of the list should be a_i, meaning that the last 
+        The i-th element of the list should be a_i, meaning that the last
         element of the list is the coefficient of the highest degree term."""
-        
-        raise NotImplemented
+        return self.coefficients
 
     def __call__(self, x):
         """Return the value of the polynomial evaluated at the number x"""
+        result = 0;
+        for index in range(len(self.coefficients)):
+            result+= self.coefficients[index]*(x**index)
 
-        raise NotImplemented
+        return result;
 
-    
     def __add__(self, p):
         """Return the polynomial which is the sum of p and this polynomial
-        Should assume p is Polynomial([p]) if p is int. 
+        Should assume p is Polynomial([p]) if p is int.
 
         If p is not an int or Polynomial, should raise ArithmeticError."""
 
         raise NotImplemented
-        
+
     def __sub__(self, p):
         """Return the polynomial which is the difference of p and this polynomial
-        Should assume p is Polynomial([p]) if p is int. 
+        Should assume p is Polynomial([p]) if p is int.
 
         If p is not an int or Polynomial, should raise ArithmeticError."""
 
@@ -53,13 +59,35 @@ class Polynomial:
         """Return the polynomial which is this polynomial multiplied by some integer"""
 
         raise NotImplemented
-    
+
     def __repr__(self):
         """Return a nice string representation of polynomial.
-        
+
         E.g.: x^6 - 5x^3 + 2x^2 + x - 1
         """
-        raise NotImplemented
+        result = ""
+        length = len(self.coefficients);
+        for index in reversed(range(length)):
+            value = self.coefficients[index];
+            if value==0:
+                continue
+
+            if value>0:
+                sign = ("+ " if index!=length-1 else "")
+            else:
+                sign = "- "
+
+            value = abs(value)
+            value = value if value!=1 or index==0 else ""
+
+            degree = ("^{}".format(index) if index>1 else "")
+            x = ("x" if index!=0 else "")
+
+            result+= sign + str(value) + x + degree + " "
+
+        return result
+
+
 
     def __eq__(self, p):
         """Check if two polynomials have the same coefficients."""
@@ -69,13 +97,13 @@ class Polynomial:
 def sample_usage():
     p = Polynomial([1, 2, 1]) # 1 + 2x + x^2
     q = Polynomial([9, 5, 0, 6]) # 9 + 5x + 6x^3
-    
-    
+
+
     print("The value of {} at {} is {}".format(p, 7, p(7)))
 
     print("The coefficients of {} are {}".format(p, p.coefficients()))
 
-    
+
     print("\nAdding {} and {} yields {}".format(p, q, p+q))
 
     p, q, r = map(Polynomial,
@@ -83,7 +111,7 @@ def sample_usage():
                       [1, 0, 1], [0, 2, 0], [1, 2, 1]
                   ]
     )
-    
+
     print("\nWill adding {} and {} be the same as {}? Answer: {}".format(
         p, q, r, p+q == r
     ))
--- assignment3/test_polynomials.py
+++ assignment3/test_polynomials.py
@@ -1,40 +1,75 @@
 #!/usr/bin/env python
+from polynomials import Polynomial
 
 def test_polynomial_evaluate_0():
     p = Polynomial([1, 2, 3])
-    assert p(0) = 1
+    assert p(0) == 1
 
 def test_polynomial_evaluate_1():
     p = Polynomial([1, 2, 3])
-    assert p(1) = 9
+    assert p(1) == 6
 
 def test_polynomial_evaluate_minus_2():
     p = Polynomial([1, 2, 3])
-    assert p(-2) = 9
+    assert p(-2) == 9
 
 def test_add_polynomials():
     p1 = Polynomial([1, 2, 3])
     p2 = Polynomial([1, 2, 3, 4])
-    assert (p1 + p2) = Polynomial([2, 4, 6, 4])
+    assert (p1 + p2) == Polynomial([2, 4, 6, 4])
 
 def test_add_polynomials_with_zeroes():
     p1 = Polynomial([0, 2, 0])
     p2 = Polynomial([1, 0, 3, 0])
-    assert (p1 + p2) = Polynomial([1, 2, 3])
+    assert (p1 + p2) == Polynomial([1, 2, 3])
 
 def test_subtract_polynomials():
     p1 = Polynomial([1, 2, 3])
     p2 = Polynomial([1, 2, 3, 4])
-    assert (p1 - p2) = Polynomial([0, 0, 0, -4])
+    assert (p1 - p2) == Polynomial([0, 0, 0, -4])
 
 def test_degree_no_zeroes():
     p = Polynomial([1, 2, 3, 4, 5])
-    assert p.degree() = 4
+    assert p.degree() == 4
 
 def test_degree_highest_zero():
     p = Polynomial([1, 2, 3, 4, 0])
-    assert p.degree() = 3
+    assert p.degree() == 3
 
 def test_degree_all_zeroes():
         p = Polynomial([0, 0, 0, 0, 0])
-        assert p.degree() = 0
+        assert p.degree() == -1
+
+def test_repr_no_zeroes():
+    p = Polynomial([1, 2, 3, 4, 5])
+    assert str(p) == "5x^4 + 4x^3 + 3x^2 + 2x + 1 "
+
+def test_repr_some_zeroes():
+    p = Polynomial([0, 2, 0, 0, 5])
+    assert str(p) == "5x^4 + 2x "
+
+def test_repr_all_zeroes():
+    p = Polynomial([0, 0, 0, 0, 0])
+    assert str(p) == ""
+
+def test_repr_negatives():
+    p = Polynomial([-1, 2, -3, 4, -5])
+    assert str(p) == "- 5x^4 + 4x^3 - 3x^2 + 2x - 1 "
+
+def test_repr_ones():
+    p = Polynomial([1, 1, 1])
+    assert str(p) == "x^2 + x + 1 "
+
+
+test_degree_no_zeroes()
+test_degree_highest_zero()
+test_degree_all_zeroes()
+test_repr_no_zeroes()
+test_repr_negatives()
+test_repr_some_zeroes()
+test_repr_all_zeroes()
+test_repr_ones()
+test_polynomial_evaluate_0()
+test_polynomial_evaluate_1()
+test_polynomial_evaluate_minus_2()
+print("All tests are passed!")
