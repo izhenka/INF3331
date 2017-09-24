778564fa986f33d2bc913b2ba07ef428691d472f
Evgenia Gurova <izhenka@gmail.com>
2017-09-24 21:43:52 +0200

    Plotting added

--- assignment4/integrator.py
+++ assignment4/integrator.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import seaborn
+
 def integrate (f, a, b, N):
     step = abs(b-a)/(N+1)
     sum = 0
@@ -5,4 +8,17 @@ def integrate (f, a, b, N):
         sum += f(index*step)*step
     return sum
 
-print(integrate(lambda x: x**2, 0, 1, 1E+5))
+def plotError():
+    x = [1, 2, 4 , 10, 20, 40, 100, 200, 400]
+    func = lambda i: abs(integrate(lambda z: z**2, 0, 1, i) - 1/3)
+    y = list(map(func, x))
+
+    plt.plot(x, y,
+    label="Quadratic error as a function of the N number of points")
+    plt.xlabel("Number points")
+    plt.ylabel("Error")
+    plt.legend()
+    plt.show()
+
+
+plotError()
