5d9287e2596ee9da615cd7668c3a35f1ee9a14c7
Evgenia Gurova <izhenka@gmail.com>
2017-10-01 00:00:37 +0200

    adds numba

---
+++ assignment4/numba_integrator.py
@@ -0,0 +1,24 @@
+import time
+from numba import jit
+
+
+@jit
+def numba_integrate (f, a, b, N):
+    N = int(N)
+    step = abs(b-a)/(N+1)
+    sum = 0
+    for index in range(1, N+2):
+        sum += f(a+index*step)
+    return sum*step
+
+@jit
+def kvadrat(x):
+    return x**2
+
+
+if __name__ == '__main__':
+    t0 = time.clock()
+    numba_integrate(lambda x: x**2, 0, 1, N=1E+7)
+    t1 = time.clock()
+
+    print('numba {:.4f} sec'.format(t1-t0))
