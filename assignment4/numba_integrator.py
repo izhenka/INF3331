5dff28fc9ad7fcf3dde9b665563c452cc34c2dd7
Evgenia Gurova <izhenka@gmail.com>
2017-10-01 17:32:04 +0200

    adds report for numba

--- assignment4/numba_integrator.py
+++ assignment4/numba_integrator.py
@@ -1,9 +1,12 @@
 import time
 from numba import jit
 
-
 @jit
-def numba_integrate (f, a, b, N):
+def f(x):
+    return x**2
+
+@jit(nopython=True)
+def numba_integrate (a, b, N):
     N = int(N)
     step = abs(b-a)/(N+1)
     sum = 0
@@ -11,14 +14,10 @@ def numba_integrate (f, a, b, N):
         sum += f(a+index*step)
     return sum*step
 
-@jit
-def kvadrat(x):
-    return x**2
-
-
 if __name__ == '__main__':
     t0 = time.clock()
-    numba_integrate(lambda x: x**2, 0, 1, N=1E+7)
+    # numba_integrate(lambda x: x**2, 0, 1, N=1E+7)
+    numba_integrate(0, 1, N=1E+7)
     t1 = time.clock()
 
     print('numba {:.4f} sec'.format(t1-t0))
