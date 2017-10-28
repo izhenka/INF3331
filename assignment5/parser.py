487b9dc5455f94208e0e86e9af22f7642115dc7b
Evgenia Gurova <izhenka@gmail.com>
2017-10-28 23:31:06 +0200

    5.2 done: images. blockquotes, wikisearch

--- assignment5/parser.py
+++ assignment5/parser.py
@@ -1,5 +1,15 @@
 import re
 
+def parse_nwodkram(text):
+    result = parse_bold(text)
+    result = parse_italic(result)
+    result = parse_escaped_tags(result)
+    result = parse_links(result)
+    result = parse_images(result)
+    result = parse_blockquotes(result)
+    result = parse_wiki_searches(result)
+    return result
+
 def parse_bold(text):
     tag = r"(?:(?<!\\)%)"
     regex_in = tag + r"((?:\\%|[^%])+)" + tag
@@ -17,7 +27,7 @@ def parse_escaped_tags(text):
     return re.sub(regex_in, r"\1", text)
 
 def parse_links(text):
-    regex_in = r"\[([^\[\]]+)\]\(((http[s]?:\/\/)?.+)\)"
+    regex_in = r"\[([^\]]+)\]\(((http[s]?:\/\/)?[^)]+)\)"
     regex_out = r"<a href='\2'>\1</a>"
     return re.sub(regex_in, formate_link, text)
 
@@ -30,30 +40,33 @@ def formate_link(matchobj):
     group2 = matchobj.group(2)
     return f"<a href='{prefix}{group2}'>{group1}</a>"
 
-def parse_nwodkram(text):
-    result = parse_bold(text)
-    result = parse_italic(result)
-    result = parse_escaped_tags(result)
-    result = parse_links(result)
-    return result
+def parse_images(text):
+    regex_in = r"<(http.+)>\(w=(\d+),h=(\d+)\)"
+    regex_out = r"<img src='\1' style='width:\2px;height:\3px' ;>"
+    return re.sub(regex_in, regex_out, text)
+
+def parse_blockquotes(text):
+    regex_in = r">>(.+)"
+    regex_out = r"<blockquote>\1</blockquote>"
+    return re.sub(regex_in, regex_out, text)
+
+def parse_wiki_searches(text):
+    regex_in = r"\[wp:(.+)\]"
+    # print(re.findall(regex_in, text))
+    regex_out = r"<a href='https://en.wikipedia.org/w/index.php?title=Special:Search&search=\1'>Search Wikipedia for \1</a>"
+    return re.sub(regex_in, regex_out, text)
+
+
 
 if __name__ == '__main__':
 
-    source_text = r"""
-    This is some Nwodkram text. Note that *this* is in italic, and %this% is in bold.
-    If you want to write an \* or an equal sign and not have the parser eat them,
-    that's easy -  note that \* this \* is not in italic even though it's between two \*s,
-    and \% this \% is not in bold.
+    source_text = """%Lorem% ipsum *dolor sit amet*, consectetur *adipiscing elit. Nullam tempor* nunc at justo tincidunt congue. %Aliquam hendrerit mollis pretium! Praesent id% mi est. [Praesent,](www.praesent.com) sed orci aliquet, dapibus elit sed, maximus dolor. Donec ut viverra velit, in sollicitudin nisl. Aliquam nec orci sit amet sem congue condimentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
 
-    [here](www.google.com) is a hyperlink.
-    [here](http://www.google.com) is another.
-    [and here](https://www.weird?$|site.weird/path/) is a third with some weird characters.
-    Follow it at your own peril.
+    >>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a nulla *eget eros euismod volutpat. Suspendisse* id luctus lorem. Vivamus non erat bibendum lacus sodales convallis scelerisque ac diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget quam eros. Nulla lectus turpis, porttitor sed laoreet id, varius eget dolor. Proin non sapien et risus dictum suscipit quis id leo. Aenean at mauris vel eros gravida gravida. Sed feugiat %molestie \*libero vel\%\% pulvinar? Sed% a accumsan risus, at vehicula felis. Nullam eget est blandit eros consectetur facilisis. Etiam ligula augue, fringilla ac nibh sit amet, posuere dignissim libero. Nunc accumsan odio leo, et mollis turpis aliquam eu. Proin sed maximus erat. Maecenas diam velit, tristique et posuere ut, placerat sit amet diam.
 
-    Ideally, it would be good if your hyperlinks can contain parentheses and underscores.
-    But don't worry too much if some weird combination is ambiguous or results in
-    weird stuff.
+    Curabitur finibus, turpis viverra rutrum consequat, ligula tortor consectetur ex, eu malesuada lacus ipsum in \%% urna. \% Fusce% in *sapien %mau\*ris.% Duis purus dui*, viverra in tellus eu, imperdiet fringilla [felis. Curabitur rhoncus tincidunt varius. Cras](inf3331.no) gravida metus ut [wp:vestibulum] vestibulum. \*Integer cursus* ex\* in rutrum volutpat*. Nunc scelerisque gravida risus sed ullamcorper. Proin [lorem,](https://www.malesuada.com) massa <https://www.mn.uio.no/astro/english/services/it/help/basic-services/latex/uiologo.gif>(w=100,h=40) quam in, scelerisque elementum arcu. Nunc scelerisque sem ac lectus porttitor, sed molestie odio *bibendum.*
     """
+
     result = parse_nwodkram(source_text)
     with open('out.html', 'w') as f:
         print(result, file=f)
