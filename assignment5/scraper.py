b1542f0037ec5f345ab5cef67cd230fa8e07d71b
Evgenia Gurova <izhenka@gmail.com>
2017-10-29 12:21:23 +0100

    5.5 done, all the emails recursively

--- assignment5/scraper.py
+++ assignment5/scraper.py
@@ -1,4 +1,5 @@
 import re
+import urllib.request
 
 def find_emails(text):
     name = server = r"[\w\.#$%&~’\*\+\-/=?‘|{}.]+"
@@ -11,3 +12,32 @@ def find_urls(text):
     regex = r"<a\s+(?:class.+)?href\s*=\s*(\"|\')(http[s]?://(?:www\.)?[a-z1-9\.\-~]+\.[a-z1-9\.\-~]+[a-z1-9/\.\-~]*)\1>"
     matches = re.findall(regex, text,flags=re.IGNORECASE)
     return list(map(lambda x: x[1], matches))
+
+def all_the_emails(url, depth):
+    emails = []
+    all_the_emails_recursive(url, depth, emails)
+    return list(set(emails))
+
+
+def all_the_emails_recursive(url, depth, all_emails):
+    response = str(urllib.request.urlopen(url).read())
+    emails = find_emails(response)
+
+    print(f"\n----depth {depth}, url '{url}'---")
+    print(f"emails:\n{emails}")
+
+    if depth == 0:
+        all_emails.extend(emails)
+        return
+
+    urls = find_urls(response)
+    print(f"urls:\n{urls}")
+
+    for inner_url in urls:
+        all_the_emails_recursive(inner_url, depth-1, all_emails)
+
+
+
+if __name__ == '__main__':
+    emails = all_the_emails("https://www.uio.no/", 1)
+    print(f"*** Result ****\nemails:\n{emails}")
