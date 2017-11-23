54cd74cb6a94982e273849e1c2a17aa07648eeb8
Evgenia Gurova <izhenka@gmail.com>
2017-11-23 21:36:32 +0100

    styling

--- assignment6/options.py
+++ assignment6/options.py
@@ -5,17 +5,61 @@ class Options:
         self.month = month
         self.y_min = y_min
         self.y_max = y_max
-
-            
-def render():
-    html = f"""
-    <form action="/newplot">
-    Year from: <input type="text" name="YearFrom" value="{year_from}"><br>
-    Year to: <input type="text" name="YearTo" value="{year_to}"><br>
-    Month(for temperature): <input type="text" name="Month" value="{month}"><br>
-    Y min: <input type="text" name="YMin" value="{y_min}"><br>
-    Y max: <input type="text" name="YMax" value="{y_max}"><br>
-    <input type="submit" value="Refresh">
-    </form>
-    """
-    return html
+        
+        
+    def render(self):
+        
+        # f"""
+        # <form action="/newplot">
+        # Year from: <input type="text" class="form-control" name="YearFrom" value="{self.year_from}"><br>
+        # Year to: <input type="text" name="YearTo" value="{self.year_to}"><br>
+        # Month(for temperature): <input type="text" name="Month" value="{self.month}"><br>
+        # Y min: <input type="text" name="YMin" value="{self.y_min}"><br>
+        # Y max: <input type="text" name="YMax" value="{self.y_max}"><br>
+        # <input type="submit" value="Refresh">
+        # </form>
+        html = f"""
+        <form action="/newplot">
+            <div class="container" style="width: 500px; margin-left: 10px; margin-top: 20px;">
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="yearFrom">Year from</label>
+                    <input type="text" class="form-control" id="yearFrom" placeholder="Enter year to plot from">
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="yearTo">Year to</label>
+                    <input type="text" class="form-control" id="yearTo" placeholder="Enter year to plot to">
+                  </div>
+                </div>
+                  <div class="form-group">
+                    <label for="month">Month(for temperature)</label>
+                    <select class="form-control" id="month">
+                      <option value="1">January</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                      <option>6</option>
+                      <option>7</option>
+                      <option>8</option>
+                      <option>9</option>
+                      <option>10</option>
+                      <option>11</option>
+                      <option>12</option>
+                    </select>
+                  </div>
+                  <div class="form-row">
+                      <div class="form-group col-md-6">
+                        <label for="yMin">Y-value min</label>
+                        <input type="text" class="form-control" id="yMin" placeholder="Enter min y-value">
+                      </div>
+                      <div class="form-group col-md-6">
+                        <label for="yMax">Y-value max</label>
+                        <input type="text" class="form-control" id="yMax" placeholder="Enter max y-value">
+                      </div>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Refresh</button>
+            </div>
+        </form>        
+        """
+        return html
--- assignment6/web_visualization.py
+++ assignment6/web_visualization.py
@@ -22,13 +22,40 @@ def render_plots(options):
     plot_CO2(default_options)
     co2_url = save_plot_and_return_url()
     
-    html = f"""
+    body = f"""
     <h1>Visualization of temperature and CO2 data</h1>
     {default_options.render()}
     <img src="{temperature_url}" alt="Temperature vs. time plot" height="400" width="1200">
     <img src="{co2_url}" alt="CO2-emission vs. time plot" height="400" width="1200">
     """
+    return render_page(body)
+
+def render_page(body):
+    html = f"""
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <title>Plots</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+      </head>
+      <body>
+        {body}
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+      </body>
+    </html>
+    """
     return html
+
 
 @app.route("/newplot")
 def redraw():
